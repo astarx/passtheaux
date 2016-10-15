323c41001c5d3c28b8b12ea8a6c93e6b01d5c572
Kevin.Sun <kevin_sun@hotmail.ca>
2016-10-15 14:57:59 -0400

    new designs

---
+++ public/assets/js/song.js
@@ -0,0 +1,9 @@
+(function() {
+
+    "use strict";
+
+    document.getElementById("seeSongs").onclick = function() {
+            location.href = "/songlist.html";
+        };
+
+})();
---
+++ public/songassets/js/return.js
@@ -0,0 +1,9 @@
+(function() {
+
+    "use strict";
+
+    document.getElementById("addSong").onclick = function() {
+        location.href = "/index.html";
+    };
+
+})();
